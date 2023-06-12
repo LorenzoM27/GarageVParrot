5928e2b21c15631afb713f3b9ff8c8312bcca611
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-12 18:02:19 +0200

    index.php css, images

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
+    <link rel="icon" href="img/favicon.png" />
+    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <title>Garage V.Parrot</title>
+</head>
+<body>
+    <header>
+        <section class="headerSection">
+            <div class="container headerLogo">
+                <a href="index.php"><img class="logo" src="img/garage-vparrot-t.png" alt="Logo Garage V.Parrot"><h1> GARAGE V.PARROT</h1>
+            </div>
+            <nav class="headerNav">
+                <ul class="container">
+                    <li><a class="contact" href="contact.php">Contact</a></li>
+                    <li><a class="opinion" href="opinion.php">Avis</a></li>
+                    <li><a class="account" href="account.php">Connexion</a></li>
+                    <li><a class="about" href="#">A propos</a></li>
+                </ul>
+            </nav>
+        </section>
+    </header>
+    <main class="activities">
+        <h2 class="services">Les services que nous proposons</h2>
+        <section class="activity">
+            <img class="activity-image" src="img/mecanique.jpeg" alt="Réparation et entretien des véhicules">
+            <div class="activity-detail">
+                <h3>Réparation carrosserie et mécanique</h3>
+                <p>Nos experts mécaniciens réparent et remplacent les pièces endomagées de votre véhicules. Notre carrossier Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque illo, dolorum quos earum maxime quod, harum dolores asperiores quidem molestias dolorem voluptatem odit voluptates laudantium!</p>
+                
+                <a class="button" href="#">Prendre rendez-vous</a>
+                
+                
+            </div>
+        </section>
 
-echo "Exécution de PHP"
+
+    </main>
+    <footer>
+
+
+    </footer>
+    <?php
+        echo "Exécution de PHP";
+    ?>
+
+
+    <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>-->
+</body>
+</html>
+
+
+
