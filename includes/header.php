94d4ba62a9cf949c66122995e465945df624b3f1
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-13 15:31:38 +0200

    includes php

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header>
+        <section class="headerSection">
+            <div class="container headerLogo">
+                <a href="index.php"><img class="logo" src="img/garage-vparrot-t.png" alt="Logo Garage V.Parrot"><h1> GARAGE V.PARROT</h1>
+            </div>
+            <nav class="headerNav">
+                <ul class="container">
+                    <li><a class="t" href="#"> </a></li>
+                    <li><a class="contact" href="contact.php">Contact</a></li>
+                    <li><a class="opinion" href="opinion.php">Avis</a></li>
+                    <li><a class="account" href="account.php">Connexion</a></li>
+                    <li><a class="about" href="#">A propos</a></li>
+                </ul>
+            </nav>
+        </section>
+    </header>
--- index.php
+++ index.php
@@ -11,22 +11,8 @@
     <title>Garage V.Parrot</title>
 </head>
 <body>
-    <header>
-        <section class="headerSection">
-            <div class="container headerLogo">
-                <a href="index.php"><img class="logo" src="img/garage-vparrot-t.png" alt="Logo Garage V.Parrot"><h1> GARAGE V.PARROT</h1>
-            </div>
-            <nav class="headerNav">
-                <ul class="container">
-                    <li><a class="t" href="#"> </a></li>
-                    <li><a class="contact" href="contact.php">Contact</a></li>
-                    <li><a class="opinion" href="opinion.php">Avis</a></li>
-                    <li><a class="account" href="account.php">Connexion</a></li>
-                    <li><a class="about" href="#">A propos</a></li>
-                </ul>
-            </nav>
-        </section>
-    </header>
+    <?php include('includes/header.php'); ?>
+    
     <main class="activities">
         <h2 class="services">Les services que nous proposons</h2>
         <section class="activity">
