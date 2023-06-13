04d5cce243ecbf2b796def1f8bdc6cdf096ff61e
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-13 15:56:58 +0200

    footer

---
+++ includes/footer.php
@@ -0,0 +1,29 @@
+<footer>
+        <div class="container footer">
+            <section class="footer_section">
+                <h4>Contact</h4>
+                <p><a href="https://goo.gl/maps/q2K9QetJXvA9TLB8A">12 rue de mon garage <br>
+                    75000 Paris</a></p>
+                <a href="tel:+0102030405" target="_blank"> 01 02 03 04 05 </a> 
+            </section>
+            <section class="footer_section">
+                <h4>Horaires d'ouverture</h4>
+                <p>Lundi: 8:45 - 12:00, 14:00 - 18:00</p>
+                <p>Mardi: 8:45 - 12:00, 14:00 - 18:00</p>
+                <p>Mercredi: 8:45 - 12:00, 14:00 - 18:00</p>
+                <p>Jeudi: 8:45 - 12:00, 14:00 - 18:00</p>
+                <p>Vendredi: 8:45 - 12:00, 14:00 - 18:00</p>
+                <p>Samedi: 8:45 - 12:00</p>
+                <p>Dimanche: Fermé</p>
+            </section>
+            <section class="footer_section">
+                <h4>Nous suivre</h4>
+                <div>
+                    <a href="https://facebook.fr" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://instagram.fr" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+                
+            </section>
+        </div>
+
+    </footer>
