d2acf8243011ea93971e78232b24857686f0108e
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-19 17:18:23 +0200

    contact

--- contact.php
+++ contact.php
@@ -1,10 +1,32 @@
+<?php
+    if(!empty($_POST["send"])) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+    
+        $toEmail = "vincent.parrot@gmail.com";
+        $mailHeaders = "From: " . $name . "<". $email .">\r\n";
+        if(mail($toEmail, $subject, $message, $mailHeaders)) {
+            $mail_msg = "Votre messag à bien été envoyé.";
+            $type_mail_msg = "success";
+        }else{
+            $mail_msg = "Une erreur est survenue, veuillez réessayer.";
+            $type_mail_msg = "error";
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="js/contact.js"></script>
     <link rel="stylesheet" href="css/common.css" type="text/css">
+    <link rel="stylesheet" href="css/contact.css" type="text/css">
     <title>Nous contacter</title>
 </head>
 <body>
@@ -12,16 +34,16 @@
     <main>
         <div id="box">
             <form id="form" enctype="multipart/form-data" onsubmit="return validate()" method="post">
-                <h3>Formulaire de contact</h3>
+                <h3>Nous contacter</h3>
                 <label>Nom: <span>*</span></label>
                 <input type="text" id="name" name="name" placeholder="Nom"/>
                 <label>Email: <span>*</span></label><span id="info" class="info"></span>
                 <input type="text" id="email" name="email" placeholder="Email"/>
                 <label>Sujet: <span>*</span></label>
-                <input type="text" id="subject" name="subject" placeholder="Demande de renseignement"/>
+                <input type="text" id="subject" name="subject" placeholder="Réparation, entretien, rendez-vous, occasion..."/>
                 <label>Message:</label>
-                <textarea id="message" name="message" placeholder="Message..."></textarea>
-                <input type="submit" name="send" value="Envoyer le message"/>
+                <textarea id="message" name="message" placeholder="Dites nous ce qui vous tracasse !"></textarea>
+                <input class="submitButton" type="submit" name="send" value="Envoyer le message"/>
                 <div id="statusMessage"> 
                     <?php if (! empty($db_msg)) { ?>
                     <p class='<?php echo $type_db_msg; ?>Message'><?php echo $db_msg; ?></p>
@@ -38,21 +60,3 @@
 </body>
 </html>
 
-<?php
-    if(!empty($_POST["send"])) {
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $subject = $_POST["subject"];
-        $message = $_POST["message"];
-    
-        $toEmail = "VotreAdresse@gmail.com";
-        $mailHeaders = "From: " . $name . "<". $email .">\r\n";
-        if(mail($toEmail, $subject, $message, $mailHeaders)) {
-            $mail_msg = "Votre messag à bien été envoyé.";
-            $type_mail_msg = "success";
-        }else{
-            $mail_msg = "Une erreur est survenue, veuillez réessayer.";
-            $type_mail_msg = "error";
-        }
-    }
-?>
