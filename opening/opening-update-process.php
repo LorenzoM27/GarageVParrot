a35fc8db15d1f9fd6f4e3f368374916e6c11c75e
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-22 16:51:05 +0200

    opening

--- opening/opening-update-process.php
+++ opening/opening-update-process.php
@@ -1,13 +1,27 @@
 <?php
 require('../registration/config.php');
-//if(count($_POST)>0) {
-mysqli_query($conn,"UPDATE horaires set id='" . $_POST['id'] . "', lundi'" . $_POST['lundi'] . "', mardi='" . $_POST['mardi'] . "', mercredi='" . $_POST['mercredi'] . "' ,jeudi='" . $_POST['jeudi'] ."', vendredi'" . $_POST['vendredi'] . "', samedi'" . $_POST['samedi'] ."', dimanche'" . $_POST['dimanche'] . "' WHERE id='" . $_POST['id'] . "'");
+if(count($_POST)>0) {
+//mysqli_query($conn,"UPDATE horaires set id='" . $_POST['id'] . "', lundi'" . $_POST['lundi'] . "', mardi='" . $_POST['mardi'] . "', mercredi='" . $_POST['mercredi'] . "' ,jeudi='" . $_POST['jeudi'] ."', vendredi'" . $_POST['vendredi'] . "', samedi'" . $_POST['samedi'] ."', dimanche'" . $_POST['dimanche'] . "' WHERE id='" . $_POST['id'] . "'");
+$id = $_POST['id'];
+$lundi = $_POST['lundi'];
+$mardi = $_POST['mardi'];
+$mercredi = $_POST['mercredi'];
+$jeudi = $_POST['jeudi'];
+$vendredi = $_POST['vendredi'];
+$samedi = $_POST['samedi'];
+$dimanche = $_POST['dimanche'];
+
+$query = "UPDATE `horaires` SET `id`='".$id."',`lundi`='".$lundi."',`mardi`='".$mardi."',`mercredi`='".$mercredi."',`jeudi`='".$jeudi."',`vendredi`='".$vendredi."',`samedi`='".$samedi."',`dimanche`='".$dimanche."' WHERE `id` = '".$id."'";
+
+$result = mysqli_query($conn, $query);
 
 $message = "Horaires modifié avec succés";
-//}
+
+}
 
 $result = mysqli_query($conn,"SELECT * FROM horaires WHERE id='" . $_GET['id'] . "'");
 $row= mysqli_fetch_array($result);
+
 ?>
 
 <!DOCTYPE html>
@@ -17,11 +31,13 @@ $row= mysqli_fetch_array($result);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/opening-process.css" type="text/css">
     <title>Modification des horaires d'ouverture</title>
 </head>
 <body>
     <?php include('../includes/admin-header.php'); ?>
     <main>
+        <h2>Modifier les horaires</h2>
         <form name="frmUser" method="post" action="">
             <p><?php if(isset($message)) { echo $message; } ?></p>
 
@@ -54,7 +70,7 @@ $row= mysqli_fetch_array($result);
                 <label>Dimanche : </label>
                 <input type="text" name="dimanche" value="<?php echo $row['dimanche']; ?>">
             </div>
-            <input type="submit" name="submit" value="Update Data" class="buttom">
+            <input type="submit" name="submit" value="Modifier les horaires" class="button">
             
            
         </form>
--- registration/config.php
+++ registration/config.php
@@ -5,10 +5,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'garage_VParrot');
  
-// Connexion à MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// Vérifier la connexion
 if($conn === false){
     die("ERREUR : Connexion impossible. " . mysqli_connect_error());
 }
