2f3b346eb95c451b83668a4fe3a447a2270cfe3d
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-26 13:20:31 +0200

    services

--- scripts/scriptDB/services.sql
+++ scripts/scriptDB/services.sql
@@ -2,5 +2,6 @@ CREATE TABLE garage_VParrot.services
 (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     services VARCHAR(100) not null,
-    servicesImage mediumblob not null
+    description VARCHAR(500) not null,
+    image mediumblob not null
 )
