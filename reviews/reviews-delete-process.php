37cf0fbe134ed99d00844b84942400edfc5a5f7f
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-30 16:09:20 +0200

    reviews

--- reviews/reviews-delete-process.php
+++ reviews/reviews-delete-process.php
@@ -2,7 +2,7 @@
     require('../registration/config.php');
     $sql = "DELETE FROM reviews WHERE id='" . $_GET["id"] . "'";
     if (mysqli_query($conn, $sql)) {
-        echo "Avis supprimé";
+        echo "Avis supprimé !";
     } else {
         echo "Une erreur c'est produite" . mysqli_error($conn);
     }
---
+++ reviews/reviews-user-add.php
@@ -0,0 +1,35 @@
+<section>
+    <?php
+        if (isset($_REQUEST['name'], $_REQUEST['score'], $_REQUEST['display'], $_REQUEST['message'])){
+                
+            $name = stripslashes($_REQUEST['name']);
+            $name = mysqli_real_escape_string($conn, $name);
+                    
+            $score = stripslashes($_REQUEST['score']); 
+            $score = mysqli_real_escape_string($conn, $score);
+                    
+            $display = stripslashes($_REQUEST['display']);
+            $display = mysqli_real_escape_string($conn, $display);
+
+            $message = stripslashes($_REQUEST['message']);
+            $message = mysqli_real_escape_string($conn, $message);
+                    
+            $query = "INSERT into `reviews` ( name, score, display, message)
+                VALUES ('$name', '$score', '$display', '$message')";
+                $res = mysqli_query($conn, $query);
+        }
+    ?>
+    <div class="form">
+         <form class="form" action="" method="post">
+            <h2>Ajouter un avis</h2>
+            <label>Nom :</label>
+            <input type="text" name="name" placeholder="Nom" required />
+            <label>Note : </label>
+            <input type="text" name="score" placeholder="Déçu, peu satisfait, satisfait, très satisfait.." required/>
+            <input type="hidden" name="display" value="non" required />
+            <label>Mon message: </label>
+            <textarea name="message" placeholder="Mon message" required></textarea>
+            <input type="submit" class="button" name="submit" value="Je donne mon avis" required/>
+        </form>
+    </div>
+</section>
