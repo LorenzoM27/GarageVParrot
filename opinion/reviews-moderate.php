b187ecb6ad00044c5a1846201e49ffc17a99d9c6
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-06-30 14:48:56 +0200

    reviews moderate

---
+++ opinion/reviews-moderate.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modérer les avis</title>
+</head>
+<body>
+    <?php include('includes/user-header.php'); ?>
+    
+</body>
+</html>
