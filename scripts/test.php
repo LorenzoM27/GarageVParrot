2159c68e297e617d0527965f3192e8fc2d962b39
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-07-05 11:09:35 +0200

    test

--- scripts/test.php
+++ scripts/test.php
@@ -9,6 +9,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
+  	<link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
 	<title>Document</title>
 </head>
 <body>
@@ -19,7 +21,7 @@
                     ?>
 
 
-<div class="your-class">
+<div class=".single-item">
 	<img class="car-image" id="slide" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image1']); ?>" alt="image voiture"/> 
 	<img class="car-image" id="slide" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image2']); ?>" alt="image voiture"/> 
 	<img class="car-image" id="slide" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image3']); ?>" alt="image voiture"/> 
@@ -34,5 +36,15 @@
                         echo "Aucun résultat";
                     }
                     ?>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="../slick/slick.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+		$('.single-item').slick();
+    });
+  </script>
 </body>
 </html>
