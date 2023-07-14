9bd40ad65152166ac0b43ffa02ebc93aea38b4e4
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2023-07-14 14:30:32 +0100

    divers

---
+++ occasion/add-car-admin.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/favicon.png" />
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/reviews.css" type="text/css">
+    <title>Véhicules d'occasion</title>
+</head>
+<body>
+    <?php include('../includes/admin-header.php'); ?>
+    <main>
+        <?php
+                require('../registration/config.php');
+                //if (isset($_REQUEST['brand'], $_REQUEST['price'], $_REQUEST['km'], $_REQUEST['year'], $_REQUEST['first_hand'], $_REQUEST['owners'], $_REQUEST['energy'], $_REQUEST['gearbox'], $_REQUEST['color'], $_REQUEST['doors'], $_REQUEST['seats'], $_REQUEST['puissance_fiscale'], $_REQUEST['puissance_chevaux'], $_REQUEST['crit_air'], $_REQUEST['consommation'], $_REQUEST['description'], $_FILES['image1'], $_REQUEST['image2'], $_REQUEST['image3'], $_REQUEST['image4'], $_REQUEST['image5'])){
+                  if (isset($_POST['submit'])) {
+                
+                $brand = strip_tags($_REQUEST['brand']);
+                $brand = mysqli_real_escape_string($conn, $brand);
+
+                $price = strip_tags($_REQUEST['price']);
+                $price = mysqli_real_escape_string($conn, $price);
+
+                $km = strip_tags($_REQUEST['km']);
+                $km = mysqli_real_escape_string($conn, $km);
+
+                $year = strip_tags($_REQUEST['year']);
+                $year = mysqli_real_escape_string($conn, $year);
+
+                $first_hand = strip_tags($_REQUEST['first_hand']);
+                $first_hand = mysqli_real_escape_string($conn, $first_hand);
+
+                $owners = strip_tags($_REQUEST['owners']);
+                $owners = mysqli_real_escape_string($conn, $owners);
+
+                $energy = strip_tags($_REQUEST['energy']);
+                $energy = mysqli_real_escape_string($conn, $energy);
+
+                $gearbox = strip_tags($_REQUEST['gearbox']);
+                $gearbox = mysqli_real_escape_string($conn, $gearbox);
+
+                $color = strip_tags($_REQUEST['color']);
+                $color = mysqli_real_escape_string($conn, $color);
+
+                $doors = strip_tags($_REQUEST['doors']);
+                $doors = mysqli_real_escape_string($conn, $doors);
+                
+                $seats = strip_tags($_REQUEST['seats']); 
+                $seats = mysqli_real_escape_string($conn, $seats);
+                
+                $puissance_fiscale = strip_tags($_REQUEST['puissance_fiscale']);
+                $puissance_fiscale = mysqli_real_escape_string($conn, $puissance_fiscale);
+
+                $puissance_chevaux = strip_tags($_REQUEST['puissance_chevaux']);
+                $puissance_chevaux = mysqli_real_escape_string($conn, $puissance_chevaux);
+
+                $crit_air = strip_tags($_REQUEST['crit_air']);
+                $crit_air = mysqli_real_escape_string($conn, $crit_air);
+
+                $consommation = strip_tags($_REQUEST['consommation']);
+                $consommation = mysqli_real_escape_string($conn, $consommation);
+
+                $description = strip_tags($_REQUEST['description']);
+                $description = mysqli_real_escape_string($conn, $description);
+
+
+                $image1 = addslashes(file_get_contents($_FILES['image1']["tmp_name"]));
+
+                $image2 = addslashes(file_get_contents($_FILES['image2']["tmp_name"]));
+                $image3 = addslashes(file_get_contents($_FILES['image3']["tmp_name"]));
+                $image4 = addslashes(file_get_contents($_FILES['image4']["tmp_name"]));
+                $image5 = addslashes(file_get_contents($_FILES['image5']["tmp_name"]));
+
+
+                
+                $query = "INSERT into `occasion_cars` (brand, price, km, year, first_hand, owners, energy, gearbox, color, doors, seats, puissance_fiscale, puissance_chevaux, crit_air, consommation, description, image1, image2, image3, image4, image5)
+                        VALUES ('$brand', '$price', '$km', '$year', '$first_hand', '$owners', '$energy', '$gearbox', '$color', '$doors', '$seats', '$puissance_fiscale', '$puissance_chevaux', '$crit_air', '$consommation', '$description', '{$image1}', '{$image2}', '{$image3}', '{$image4}', '{$image5}')";
+                $res = mysqli_query($conn, $query);
+                 }
+            ?>
+            <div class="form">
+                <form class="form" action="" method="post" enctype="multipart/form-data">
+                    <h2>Ajouter une annonce</h2>
+
+                    <label>Marque :</label>
+                    <input type="text" name="brand" required />
+
+                    <label>Prix : </label>
+                    <input type="text" name="price" required/>
+
+                    <label>Kilometrage :</label>
+                    <input type="text" name="km" required />
+
+                    <label>Année :</label>
+                    <input type="text" name="year" required />
+
+                    <label>Première main :</label>
+                    <input type="text" name="first_hand" required />
+
+                    <label>Nombre de propriétaires :</label>
+                    <input type="text" name="owners" required />
+
+                    <label>Energie :</label>
+                    <input type="text" name="energy" required />
+
+                    <label>Boîte de vitesse :</label>
+                    <input type="text" name="gearbox" required />
+
+                    <label>Couleur :</label>
+                    <input type="text" name="color" required />
+
+                    <label>Nombre de portes :</label>
+                    <input type="text" name="doors" required />
+
+                    <label>Nombre de places :</label>
+                    <input type="text" name="seats" required />
+
+                    <label>Puissance fiscale :</label>
+                    <input type="text" name="puissance_fiscale" required />
+
+                    <label>Puissance chevaux :</label>
+                    <input type="text" name="puissance_chevaux" required />
+
+                    <label>Crit'Air :</label>
+                    <input type="text" name="crit_air" required />
+
+                    <label>Consommation :</label>
+                    <input type="text" name="consommation" required />
+
+                    <label>Description : </label>
+                    <textarea name="description" placeholder="Description, options du véhicule..." required></textarea>
+
+                    <label>Image 1 : </label>
+                    <input type="file" name="image1" accept="image/*" required>
+
+                    <label>Image 2 : </label>
+                    <input type="file" name="image2" accept="image/*" required>
+
+                    <label>Image 3 : </label>
+                    <input type="file" name="image3" accept="image/*" required>
+
+                    <label>Image 4 : </label>
+                    <input type="file" name="image4" accept="image/*" required>
+
+                    <label>Image 5 : </label>
+                    <input type="file" name="image5" accept="image/*" required>
+
+
+                    <input type="submit" class="button" name="submit" value="Mettre en ligne" required/>
+                </form>
+            </div>
+    </main>
+</body>
+</html>
---
+++ reviews/reviews-moderate-admin.php
@@ -0,0 +1,59 @@
+<?php 
+    require('../registration/config.php');
+    $result = mysqli_query($conn,"SELECT * FROM reviews");
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/reviews.css" type="text/css">
+    <title>Modérer les avis</title>
+</head>
+<body>
+    <?php include('../includes/admin-header.php'); ?>
+    <main>
+        
+        <section class="reviews-section">
+        <h2>Modérer les avis</h2>
+            <div class="reviews-list">
+                <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        $i=0;
+                        while($row = mysqli_fetch_array($result)) {
+                ?>
+                    
+                    <div class="reviews">
+                        <p><strong class="reviews-name"><?php echo strip_tags($row["name"]);?></strong></p>
+                        <p><?php echo $row["score"];?></p>
+                        <p class="message"><?php echo $row["message"];?></p>
+                        <div class="reviews-list-update">
+                            <a class="button" href="reviews-update-process.php?id=<?php echo $row["id"]; ?>">Modérer cette avis</a>
+                            <a class="button" href="reviews-delete-process.php?id=<?php echo $row["id"]; ?>">Supprimer cet avis</a>
+                        </div>     
+                    </div>
+                    
+            
+                    
+                    <?php
+                        $i++;
+                        }
+                        }
+                        else{
+                            echo "Aucun résultat";
+                        }
+                    ?>
+            
+            </div>
+           
+        </section>
+        <?php include('reviews-user-add.php'); ?>
+
+    </main>
+    
+</body>
+</html>
